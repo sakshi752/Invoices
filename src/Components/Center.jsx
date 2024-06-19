d2b41554b42361996ffc988eaaf4c5ee7907c28d
Sakshi <saks0705@gmail.com>
2024-06-19 23:02:16 +0530

    initial commit

--- src/Components/Center.jsx
+++ src/Components/Center.jsx
@@ -7,23 +7,28 @@ const Center = () => {
     const [isDropDown, setIsDropDown] = useState(false);
     const [toggleForm, setToggleForm] = useState(false);
 
-    const transition={ duration: 0.3, type: 'tween' }
+    const transition = {
+        type: "spring",
+        stiffness: 200,
+        damping: 15,  // Adjust damping to control bounciness
+        duration: 1
+    }
 
     return (
         <div>
             <div className='dark:bg-[#141625] scrollbar-hide duration-300 min-h-screen bg-[#f8f8fb] py-[34px] px-2 md:px-8 lg:px-12 lg:py-[72px] w-full'>
                 <div className='max-w-4xl flex flex-col mx-auto my-auto'>
                     {/* header */}
-                    <div className='w-full flex items-center justify-between border-b pb-4 border-[#7C5DFA]'>
+                    <div className='w-full flex flex-col md:flex-row items-center justify-between border-b pb-4 border-[#7C5DFA] gap-7'>
                         {/* left */}
-                        <div className='flex flex-col gap-2'>
+                        <div className='flex flex-col gap-2 items-center md:items-start'>
                             <h1 className='dark:text-white text-3xl md:text-4xl font-semibold tracking-widest'>Invoices</h1>
                             <p className='text-gray-500 text-sm'>There are 7 total invoices.</p>
                         </div>
                         {/* right */}
                         <div className='flex justify-between items-center gap-5 md:gap-7'>
                             {/* filter button */}
-                            <div className='flex items-center gap-1 md:gap-2'>
+                            <div className='flex items-center gap-1 md:gap-2 relative'>
                                 {/* filter */}
                                 <p className=' dark:text-white font-medium'>
                                     Filter
@@ -37,6 +42,16 @@ const Center = () => {
                                 >
                                     <IoIosArrowDown className='text-[#7C5DFA] font-bold text-lg' />
                                 </motion.div>
+                                {
+                                    isDropDown && (
+                                        <div className='absolute top-10 text-white bg-[#7C5DFA]  shadow-2xl px-2 py-1 rounded'>
+                                            <p>all</p>
+                                            <p>paid</p>
+                                            <p>pending</p>
+                                            <p>draft</p>
+                                        </div>
+                                    )
+                                }
                             </div>
                             {/* add invoice button */}
                             <div>
